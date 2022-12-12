b887a324d1b891a565b1c7ece5bb6c488be8a459
shahpriyansh9 <115045289+shahpriyansh9@users.noreply.github.com>
2022-12-12 11:15:42 -0500

    Add files via upload

---
+++ hw08/edu/umb/cs680/hw08/test/src/edu/umb/cs680/hw08/LinkTest.java
@@ -0,0 +1,65 @@
+package edu.umb.cs680.hw08;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import edu.umb.cs680.hw08.Directory;
+import edu.umb.cs680.hw08.File;
+import edu.umb.cs680.hw08.Link;
+
+public class LinkTest 
+{
+    static LocalDateTime localTime = LocalDateTime.now();
+  
+    Directory root = new Directory(null, "Root", 0, localTime);
+    Directory Apps = new Directory(root, "Apps", 0, localTime);
+    Directory bin = new Directory(root, "bin", 0, localTime);
+    Directory home = new Directory(root, "home", 0, localTime);
+    File a = new File(home, "a", 10, localTime);
+    File b = new File(home, "b", 20, localTime);
+    File c = new File(bin, "c", 30, localTime);
+    File x = new File(Apps, "x", 40, localTime);
+    File y = new File(Apps, "y", 50, localTime);
+    Link d = new Link(bin, "d", 0, localTime, Apps);
+    Link e = new Link(home, "e", 0, localTime, x);
+    Link f = new Link(home, "f", 0, localTime, d);
+    
+    Directory pictures = new Directory(home, "pictures", 0, localTime);
+
+    private String[] linkToStringArray(Link l) 
+    {
+		String[] linkInfo = { String.valueOf(l.isLink()), l.getName(), String.valueOf(l.getSize()),
+				String.valueOf(l.getCreationTime()), l.getParent().getName(), l.getTarget().getName() };
+		return linkInfo;
+   }
+
+
+   @Test
+   public void verifyLinkEquality() 
+   {
+       String[] expected = { "true", "d", "0", localTime.toString(), "bin", "Apps" };
+       Link actualLink = d;
+       String[] actual = linkToStringArray(actualLink);
+       assertArrayEquals(expected, actual);
+   }
+
+   @Test
+   public void verifyLinkName() 
+   {
+       String expected = "f";
+       String actual = f.getName();
+       assertEquals(expected, actual);
+   }
+
+   @Test
+	public void isDirectory() {
+		assertFalse(e.isDirectory());
+	}
+	
+
+  
+    
+}
